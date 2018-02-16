6a151bcc3deb70908c821bfd58ebfb8a36479eba
Calum <cgboal521@gmail.com>
2018-02-16 19:07:56 +0000

    .gitignore fix

--- C2/settings/dev.py
+++ C2/settings/dev.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!7
+# SECURITY WARNING: keep the secret key used in production secret!14
 SECRET_KEY = '-y6^wb7bh6q3r1(ef&74)f)pv_y6f5$a7abx8$_1ogapp^q8$('
 
 # SECURITY WARNING: don't run with debug turned on in production!
