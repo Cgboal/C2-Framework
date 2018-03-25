c3917ec725c49211a0603577c80403529aa05fda
Calum <cgboal521@gmail.com>
2018-03-26 00:15:33 +0100

    Right, this time

--- nginx-image/start.sh
+++ nginx-image/start.sh
@@ -8,14 +8,14 @@ echo build starting nginx config
 echo replacing ___my.example.com___/$MY_DOMAIN_NAME
 echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR
 echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT
-echo replacing ___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR
-echo replacing ___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT
+echo replacing ___APPLICATION_IP___/$APP_PORT_8000_TCP_ADDR
+echo replacing ___APPLICATION_PORT___/$APP_PORT_8000_TCP_PORT
 
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
 # Add your app's container IP and port into config
-sed -i "s/___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR/g" /etc/nginx/nginx.conf
-sed -i "s/___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
+sed -i "s/___APPLICATION_IP___/$APP_PORT_8000_TCP_ADDR/g" /etc/nginx/nginx.conf
+sed -i "s/___APPLICATION_PORT___/$APP_PORT_8000_TCP_PORT/g" /etc/nginx/nginx.conf
 sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR/g" /etc/nginx/nginx.conf
 sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
 
@@ -55,8 +55,8 @@ echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR
 echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT
 echo replacing ___LETSENCRYPT_HTTPS_IP___/$LETSENCRYPT_PORT_443_TCP_ADDR
 echo replacing ___LETSENCRYPT_HTTPS_PORT___/$LETSENCRYPT_PORT_443_TCP_PORT
-echo replacing ___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR
-echo replacing ___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT
+echo replacing ___APPLICATION_IP___/$APP_PORT_8000_TCP_ADDR
+echo replacing ___APPLICATION_PORT___/$APP_PORT_8000_TCP_PORT
 
 
 # Put your domain name into the nginx reverse proxy config.
@@ -64,15 +64,15 @@ sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx-secure.conf
 
 # Add LE container IP and port into config
 sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
-sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TC1P_PORT/g" /etc/nginx/nginx-secure.conf
 sed -i "s/___LETSENCRYPT_HTTPS_IP___/$LETSENCRYPT_PORT_443_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
 sed -i "s/___LETSENCRYPT_HTTPS_PORT___/$LETSENCRYPT_PORT_443_TCP_PORT/g" /etc/nginx/nginx-secure.conf
 
 # Add your app's container IP and port into config
-sed -i "s/___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
-sed -i "s/___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___APPLICATION_IP___/$APP_PORT_8000_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___APPLICATION_PORT___/$APP_PORT_8000_TCP_PORT/g" /etc/nginx/nginx-secure.conf
 
-#go!
+#go!5
 kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
 cp /etc/nginx/nginx-secure.conf /etc/nginx/nginx.conf
 
