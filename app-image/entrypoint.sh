2030cdc215a0b609ee85d9f88941d25f0c72cb90
Calum <cgboal521@gmail.com>
2018-03-26 02:49:57 +0100

    Oh the little things

--- app-image/entrypoint.sh
+++ app-image/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 /venv/bin/python manage.py makemigrations --noinput
 /venv/bin/python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('$DJANGO_ADMIN_USER', 'admin@example.com', '$DJANGO_ADMIN_PASS')" | /venv/bin/python manage.py shell
