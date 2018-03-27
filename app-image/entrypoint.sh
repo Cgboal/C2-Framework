491e891a946df4eed77e14c22f19f9a8d2464c65
Calum <cgboal521@gmail.com>
2018-03-27 02:02:37 +0100

    Wow. Okay. I guess we'll add a sleep because that'll solve not migrating?

--- app-image/entrypoint.sh
+++ app-image/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+/bin/sleep 10;
 /venv/bin/python manage.py makemigrations --noinput
 /venv/bin/python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('$DJANGO_ADMIN_USER', 'admin@example.com', '$DJANGO_ADMIN_PASS')" | /venv/bin/python manage.py shell
