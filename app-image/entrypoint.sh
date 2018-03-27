5235a07a64cbd5ab29ac3e04e6d5b3449787b4e2
Calum <cgboal521@gmail.com>
2018-03-27 04:19:39 +0100

    Added new docker container to build python2 version of agent.

--- app-image/entrypoint.sh
+++ app-image/entrypoint.sh
@@ -3,7 +3,4 @@
 /venv/bin/python manage.py makemigrations --noinput
 /venv/bin/python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('$DJANGO_ADMIN_USER', 'admin@example.com', '$DJANGO_ADMIN_PASS')" | /venv/bin/python manage.py shell
-cd /code/agent/
-/venv/bin/python setup.py bdist_wheel
-cd /code/
 /venv/bin/uwsgi --http-auto-chunked --http-keepalive
