9d2f936b7b6dfd24808e50c3e12752ec1e129c56
Calum <cgboal521@gmail.com>
2018-03-27 01:49:14 +0100

    Fixed issue with running web server, added test for checking if the webserver is running correctly, and finally mapped letsencrypt certs to an absolute path so they survive upgrades.

--- app-image/entrypoint.sh
+++ app-image/entrypoint.sh
@@ -4,4 +4,5 @@
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('$DJANGO_ADMIN_USER', 'admin@example.com', '$DJANGO_ADMIN_PASS')" | /venv/bin/python manage.py shell
 cd /code/agent/
 /venv/bin/python setup.py bdist_wheel
+cd /code/
 /venv/bin/uwsgi --http-auto-chunked --http-keepalive
