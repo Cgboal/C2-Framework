dabb7efd913b537b8822ea3450fbd61f5074ac22
Calum <cgboal521@gmail.com>
2018-03-27 01:21:20 +0100

    Added docker CI script

--- app-image/entrypoint.sh
+++ app-image/entrypoint.sh
@@ -2,4 +2,6 @@
 /venv/bin/python manage.py makemigrations --noinput
 /venv/bin/python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('$DJANGO_ADMIN_USER', 'admin@example.com', '$DJANGO_ADMIN_PASS')" | /venv/bin/python manage.py shell
+cd /code/agent/
+/venv/bin/python setup.py bdist_wheel
 /venv/bin/uwsgi --http-auto-chunked --http-keepalive
