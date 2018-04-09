dee4a85d3797278898bd80a0183e7fdf4cfb3d07
Calum <cgboal521@gmail.com>
2018-04-09 18:40:54 +0100

    Tried to be smart, failed

--- app-image/api/admin.py
+++ app-image/api/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import api.modules
 from django.contrib import admin
 from api.models import Command, Group, Agent, Agent_Group, Module, Agent_Module
-from api.helpers import get_module_models, import_modules
+from api.modules.nmap import NmapResult
 # Register your models here.
 admin.site.register(Command)
 admin.site.register(Group)
@@ -11,5 +11,4 @@ admin.site.register(Agent)
 admin.site.register(Agent_Group)
 admin.site.register(Module)
 admin.site.register(Agent_Module)
-for name, model in get_module_models(import_modules(api.modules)).items():
-    admin.site.register(model)
+admin.site.register(NmapResult)
