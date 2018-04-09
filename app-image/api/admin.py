c15f056190fb5c8d32ef03a5c318b46849f6ba2a
Calum <cgboal521@gmail.com>
2018-04-09 18:31:38 +0100

    Dynamically register all module tables in admin interface

--- app-image/api/admin.py
+++ app-image/api/admin.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import api.modules
 from django.contrib import admin
 from api.models import Command, Group, Agent, Agent_Group, Module, Agent_Module
-
+from api.helpers import get_module_models, import_modules
 # Register your models here.
 admin.site.register(Command)
 admin.site.register(Group)
@@ -11,3 +11,5 @@ admin.site.register(Agent)
 admin.site.register(Agent_Group)
 admin.site.register(Module)
 admin.site.register(Agent_Module)
+for model in get_module_models(import_modules(api.modules)):
+    admin.site.register(model)
