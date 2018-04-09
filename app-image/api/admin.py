007ad0ed85cb602bd361d7e778ed3f221e43e241
Calum <cgboal521@gmail.com>
2018-04-09 17:20:49 +0100

    Registered Agent_Module to the admin template.

--- app-image/api/admin.py
+++ app-image/api/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from api.models import Command, Group, Agent, Agent_Group, Module
+from api.models import Command, Group, Agent, Agent_Group, Module, Agent_Module
 
 # Register your models here.
 admin.site.register(Command)
@@ -10,3 +10,4 @@ admin.site.register(Group)
 admin.site.register(Agent)
 admin.site.register(Agent_Group)
 admin.site.register(Module)
+admin.site.register(Agent_Module)
