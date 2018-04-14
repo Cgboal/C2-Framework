e2b91c9896293aedac61f202271719310e2d07ad
cgboal <cgboal521@gmail.com>
2018-04-14 18:08:01 +0100

    Nice! Assimilation now available

--- app-image/assimilate.sh
+++ app-image/assimilate.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 kill -15 $(cat /var/run/uwsgi.pid);
-/bin/sleep 1;
 /venv/bin/python manage.py makemigrations --noinput
 /venv/bin/python manage.py migrate --noinput
 /venv/bin/python manage.py collectmodules
