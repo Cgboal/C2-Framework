a6b0558a6a53fc140ceffcbabf8f4aa685b61758
cgboal <cgboal521@gmail.com>
2018-04-14 19:54:47 +0100

    ??????????????

--- app-image/api/admin.py
+++ app-image/api/admin.py
@@ -3,8 +3,7 @@ from __future__ import unicode_literals
 import api.modules
 from django.contrib import admin
 from api.models import Command, Group, Agent, Agent_Group, Module, Agent_Module
-from api.modules.nmap import NmapResult
-from api.modules.hello_world import HelloWorldResult
+
 # Register your models here.
 admin.site.register(Command)
 admin.site.register(Group)
@@ -12,5 +11,3 @@ admin.site.register(Agent)
 admin.site.register(Agent_Group)
 admin.site.register(Module)
 admin.site.register(Agent_Module)
-admin.site.register(NmapResult)
-admin.site.register(HelloWorldResult)
