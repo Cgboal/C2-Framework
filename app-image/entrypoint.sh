b09116087e9ebd23a8a80bd525644323e87e949b
cgboal <cgboal521@gmail.com>
2018-04-14 19:56:38 +0100

    ??????????????

--- app-image/entrypoint.sh
+++ app-image/entrypoint.sh
@@ -3,6 +3,7 @@
 echo > /code/api/modules/__init__.py
 echo > /code/api/migrations__init__.py
 /venv/bin/python manage.py makemigrations --noinput
+/venv/bin/python manage.py makemigrations api --noinput
 /venv/bin/python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('$DJANGO_ADMIN_USER', 'admin@example.com', '$DJANGO_ADMIN_PASS')" | /venv/bin/python manage.py shell
 /venv/bin/python manage.py collectmodules
