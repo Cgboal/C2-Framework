76ea500bbaec3818beb9b873aa046fe584bf19d5
Cgboal <Cgboal521@gmail.com>
2018-04-16 19:13:00 +0100

    Changed all urls to use path object.

--- app-image/dashboard/urls.py
+++ app-image/dashboard/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url, include
 from django.urls import path
 from dashboard.views import IndexView, LoginView, command_view, GroupCreateView, logout_view, GroupView, \
     ModuleCreateView, ModuleView, RunView, AgentView
@@ -7,14 +6,14 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view()),
-    url(r'^login/', LoginView.as_view()),
-    url(r'^logout/', logout_view),
-    url(r'^command/', command_view),
-    url(r'^group/create', login_required(GroupCreateView.as_view())),
-    url(r'^group/(?P<group_id>\w+)/$', login_required(GroupView.as_view())),
-    url(r'^module/add', login_required(ModuleCreateView.as_view())),
-    path(r'^module/<uuid:module_id>/$', login_required(ModuleView.as_view())),
-    url(r'^run/(?P<group_id>\w+)/$', login_required(RunView.as_view())),
-    path(r'^agent/<uuid:agent_id>/$', login_required(AgentView.as_view()))
+    path('', IndexView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', logout_view),
+    path('command/', command_view),
+    path('group/create/', login_required(GroupCreateView.as_view())),
+    path('group/<int:group_id>/', login_required(GroupView.as_view())),
+    path('module/add/', login_required(ModuleCreateView.as_view())),
+    path('module/<uuid:module_id>/', login_required(ModuleView.as_view())),
+    path('run/<int:group_id>/', login_required(RunView.as_view())),
+    path('agent/<uuid:agent_id>/', login_required(AgentView.as_view()))
 ]
