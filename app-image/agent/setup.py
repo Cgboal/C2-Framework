4ca54ac6e853191d3c63b35a39f42924e1ccfa9b
Cgboal <Cgboal521@gmail.com>
2018-04-16 20:15:20 +0100

    Added more logging to the agent

--- app-image/agent/setup.py
+++ app-image/agent/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="C2F_Agent",
-      version="0.1.3",
+      version="0.1.5",
       description="The agent daemon component of the Command and Control framework",
       url="https://git.veldt.me/cgboal/C2-Framework",
       author="Calum Boal",
