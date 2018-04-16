639b741e2733e28eee2cf272b6d063180d6ff7f9
cgboal <cgboal521@gmail.com>
2018-04-16 16:58:18 +0100

    Okay so have to use a uuid regex

--- app-image/dashboard/urls.py
+++ app-image/dashboard/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^group/create', login_required(GroupCreateView.as_view())),
     url(r'^group/(?P<group_id>\w+)/$', login_required(GroupView.as_view())),
     url(r'^module/add', login_required(ModuleCreateView.as_view())),
-    url(r'^module/(?P<module_id>\w+)/$', login_required(ModuleView.as_view())),
+    url(r'^module/(?P<module_id>\[0-9a-f-]+)/$', login_required(ModuleView.as_view())),
     url(r'^run/(?P<group_id>\w+)/$', login_required(RunView.as_view())),
-    url(r'^agent/(?P<agent_id>\w+)/$', login_required(AgentView.as_view()))
+    url(r'^agent/(?P<agent_id>\[0-9a-f-]+)/$', login_required(AgentView.as_view()))
 ]
