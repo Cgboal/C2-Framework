c1fad0fd932f7c4714447bffaff836498f9b7163
cgboal <cgboal521@gmail.com>
2018-04-18 18:54:27 +0100

    Maybe now?

--- app-image/dashboard/views.py
+++ app-image/dashboard/views.py
@@ -203,7 +203,7 @@ class ReportView(View):
                     context["reports"][module.name]["tables"][table.name] = {}
                     context["reports"][module.name]["tables"][table.name]["name"] = table.name
                     context["reports"][module.name]["tables"][table.name]["columns"] = [f.name for f in model._meta.get_fields()]
-                    context["reports"][module.name]["tables"][table.name]["entries"] = serializers.serialize("python", model.objects.filter(agent_id__in=agents))
+                    context["reports"][module.name]["tables"][table.name]["entries"] = model.objects.filter(agent_id__in=agents).values()
 
         return render(request, template_name='report.html', context=context)
 
